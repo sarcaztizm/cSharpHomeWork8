f27cbd93581861d25903f326f7ccadc4aeccb04e
Eric <ericismag@outlook.com>
2022-10-10 01:14:43 +0300

    spiral

---
+++ Ex5_Spiral/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+Console.Clear();
+
+int [,]  InputMatrix(int [,] matrix)
+{
+    int row = 0;
+    int col = 0;
+    int dx = 1;
+    int dy = 0;
+    int dirChanges = 0;
+    int visits = 4;
+ 
+    for (int i = 0; i < matrix.Length; i++) 
+    {
+        matrix[row, col] = i + 1;
+        if (--visits == 0) 
+        {
+            visits = 4 * (dirChanges %2) + 4 * ((dirChanges + 1) %2) - (dirChanges/2 - 1) - 2;
+            int temp = dx;
+            dx = -dy;
+            dy = temp;
+            dirChanges++;
+        }
+ 
+        col += dx;
+        row += dy;
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[4, 4];
+InputMatrix(matrix);
+PrintMatrix(matrix);
